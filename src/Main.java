66c721774da84e46adc2ba8ac57ac25156af7742
JuliaNogueiradaSilva <julia.nogueiradasilva01@gmail.com>
2026-05-09 21:09:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,16 @@
+import service.FuncionarioService;
+import view.MenuPrincipal;
 
+public class Main {  // o main decide quando o sistema inicia
+
+    public static void main(String[] args) {
+
+        FuncionarioService service = new FuncionarioService(); //  um espaço na memória do computador é criado para guardar todos os funcionários que serão manipulados enquanto o programa estiver aberto
+
+        service.carregarDadosIniciais(); // o main chama esse metodo e pega os objetos prontos
+        // service indica o endereço que o metodo carregardadosIniciais esta
+
+        new MenuPrincipal(service); // liga a intergace grafica 
+                                    // service é o objeto que contém os funcionario; O Service decide como os dados são guardados.
+    }
+}
